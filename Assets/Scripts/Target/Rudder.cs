5f47c23e19c83ed0bf034b3f6506a3f645c9c55d
Denis G. Labrecque <denis.g.labrecque@gmail.com>
2020-08-16 18:21:45 -0500

    fix the rudder when ship goes backwards

--- Assets/Scripts/Target/Rudder.cs
+++ Assets/Scripts/Target/Rudder.cs
@@ -1,23 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using DGL.Math;
+using System;
 
+/// <summary>
+/// An effector that deflects force underwater.
+/// </summary>
 public class Rudder : MonoBehaviour
 {
    [Tooltip("How much steering force the rudder actually has")]
-   [SerializeField][Range(0, 1)] float m_Effect = 0.1f;
+   [SerializeField][Range(0, 1)] float _effect = 0.1f;
 
    [Tooltip("Speed the rudder reaches full effect")]
-   [SerializeField] [Range(0, 1)] float m_ShiftSpeed = 0.5f;
+   [SerializeField] [Range(0, 1)] float _shiftSpeed = 0.5f;
 
-   Rigidbody m_Rigidbody;
-   Vehicle m_Ship;
-   private float m_AngleOfAttack = 0;
-   private float m_PercentAOA = 0;
-   private float m_ActualAOA = 0;
+   Rigidbody _rigidbody;
+   Vehicle _ship;
+   private float _angleOfAttack = 0;
+   private float _percentAoa = 0;
+   private float _actualAoa = 0;
 
    // A speed that ships generally will never exceed
-   public const float GENERAL_MAX_SPEED = 200f;
+   public const float GeneralMaxSpeed = 200f;
 
    // Start is called before the first frame update
    void Awake()
@@ -27,30 +30,30 @@ public class Rudder : MonoBehaviour
 
    private void Update()
    {
-      m_ActualAOA = Mathf.Lerp(m_ActualAOA, m_PercentAOA, m_ShiftSpeed * Time.deltaTime);
+      _actualAoa = Mathf.Lerp(_actualAoa, _percentAoa, _shiftSpeed * Time.deltaTime);
    }
 
    public void GetParentItems()
    {
-      m_Rigidbody = GetComponentInParent<Rigidbody>();
-      m_Ship = GetComponentInParent<Vehicle>();
+      _rigidbody = GetComponentInParent<Rigidbody>();
+      _ship = GetComponentInParent<Vehicle>();
    }
 
    private void FixedUpdate()
    {
       // Find the total direction the ship is going as compared to the rudder
-      Vector3 localVelocity = transform.InverseTransformDirection(m_Rigidbody.velocity);
-      m_AngleOfAttack = Vector3.Angle(new Vector3(0, 0, 1), localVelocity);
+      Vector3 localVelocity = transform.InverseTransformDirection(_rigidbody.velocity);
+      _angleOfAttack = Vector3.Angle(new Vector3(0, 0, 1), localVelocity);
       // Vector3.Angle always returns a positive value, so add the sign back in
-      m_AngleOfAttack *= Mathf.Sign(localVelocity.x);
+      _angleOfAttack *= Mathf.Sign(localVelocity.x);
 
-      m_PercentAOA = DGL.Math.Utility.Percent(m_AngleOfAttack, 90f, DGL.Math.PercentMode.ClampNegative1To1);
-      float percentSpeed = DGL.Math.Utility.Percent(m_Ship.ForwardSpeed, GENERAL_MAX_SPEED, DGL.Math.PercentMode.Clamp0To1);
-      float massSquared = m_Rigidbody.mass * m_Rigidbody.mass;
+      _percentAoa = Utility.Percent(_angleOfAttack, 90f, PercentMode.ClampNegative1To1);
+      float percentSpeed = Utility.Percent(_ship.ForwardSpeed, GeneralMaxSpeed, PercentMode.ClampNegative1To1); // Negative speeds are backwards
+      float massSquared = _rigidbody.mass * _rigidbody.mass;
 
       // Debug.Log("Force: " + (speedPercent * m_AngleOfAttack));
 
-      m_Rigidbody.AddRelativeTorque(new Vector3(0, 1, 0) * m_Effect * m_ActualAOA * massSquared * percentSpeed * Time.deltaTime, ForceMode.Force); // The actual turning
-      m_Rigidbody.AddRelativeTorque(new Vector3(0, 0, 1) * m_Effect * m_ActualAOA * m_Rigidbody.mass * percentSpeed * Time.deltaTime, ForceMode.Force); // Turning effect
+      _rigidbody.AddRelativeTorque(new Vector3(0, 1, 0) * _effect * _actualAoa * massSquared * Math.Abs(percentSpeed) * Time.deltaTime, ForceMode.Force); // The actual turning
+      _rigidbody.AddRelativeTorque(new Vector3(0, 0, 1) * _effect * _actualAoa * _rigidbody.mass * percentSpeed * Time.deltaTime, ForceMode.Force); // Turning effect
    }
 }
