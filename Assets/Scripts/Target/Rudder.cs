da20ccd6cf056f1749cf818c5b87bf8921a477b1
Denis G. Labrecque <denis.g.labrecque@gmail.com>
2020-09-26 18:15:42 -0500

    Fix a bug in rudders when going backwards

--- Assets/Scripts/Target/Rudder.cs
+++ Assets/Scripts/Target/Rudder.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿
+
+using UnityEngine;
 using DGL.Math;
 using System;
 
@@ -43,7 +45,11 @@ public class Rudder : MonoBehaviour
    {
       // Find the total direction the ship is going as compared to the rudder
       Vector3 localVelocity = transform.InverseTransformDirection(_rigidbody.velocity);
-      _angleOfAttack = Vector3.Angle(new Vector3(0, 0, 1), localVelocity);
+      if (localVelocity.z > 0) // Forwards
+         _angleOfAttack = Vector3.Angle(new Vector3(0, 0, 1), localVelocity);
+      else // Backwards
+         _angleOfAttack = Vector3.Angle(new Vector3(0, 0, -1), localVelocity);
+
       // Vector3.Angle always returns a positive value, so add the sign back in
       _angleOfAttack *= Mathf.Sign(localVelocity.x);
 
@@ -51,11 +57,9 @@ public class Rudder : MonoBehaviour
       float percentSpeed = Utility.Percent(_ship.ForwardSpeed, GeneralMaxSpeed, PercentMode.ClampNegative1To1); // Negative speeds are backwards
       float massSquared = _rigidbody.mass * _rigidbody.mass;
 
-      // Debug.Log("Force: " + (speedPercent * m_AngleOfAttack));
-
       // The actual turning; speed is absolute because going backwards flips control direction
       _rigidbody.AddRelativeTorque(new Vector3(0, 1, 0) * _effect * _actualAoa * massSquared * Math.Abs(percentSpeed) * Time.deltaTime, ForceMode.Force);
       // Turning effect; should help tilt the boat, but not required
-      _rigidbody.AddRelativeTorque(new Vector3(0, 0, 1) * _effect * _actualAoa * _rigidbody.mass * Math.Abs(percentSpeed) * Time.deltaTime, ForceMode.Force);
+      _rigidbody.AddRelativeTorque(new Vector3(0, 0, 1) * 100000 * _actualAoa * _rigidbody.mass * Math.Abs(percentSpeed) * Time.deltaTime, ForceMode.Force);
    }
 }
