5164489040a6892b382593733a9891d620bed118
Denis G. Labrecque <denis.g.labrecque@gmail.com>
2020-09-26 19:36:22 -0500

    Added buoys

---
+++ Assets/Gravitate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Just add this script to something you want to gravitate.
+/// </summary>
+public class Gravitate : MonoBehaviour
+{
+   Rigidbody _rigidbody;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      _rigidbody = GetComponentInChildren<Rigidbody>();
+   }
+
+   void FixedUpdate()
+   {
+      Gravity.Gravitate(_rigidbody);
+   }
+}
